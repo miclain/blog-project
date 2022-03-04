fecdd492f0ca67220fa8fd546be22ddbdcd398ba
michele clain <michele.thays@gmail.com>
2022-03-03 21:04:25 -0300

    add edit and update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,28 @@ class ArticlesController < ApplicationController
     @article = Article.new
 	end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
 	def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article was created successfully."
-      redirect_to article_path(@article)
+      redirect_to @article
     else
       render 'new'
     end
 	end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
 end
