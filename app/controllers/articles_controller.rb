b01ba5ce05b694129dbd345301df95822ba10313
michele clain <michele.thays@gmail.com>
2022-03-05 19:54:56 -0300

    Revert "refactoring"
    This reverts commit 8860c0ff909e2dde69896d5c17728b61d6ee4092.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,10 +13,11 @@ class ArticlesController < ApplicationController
 	end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
 	def create
-    @article = Article.new(article_params)
+    @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -26,7 +27,8 @@ class ArticlesController < ApplicationController
 	end
 
   def update
-    if @article.update(article_params)
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -35,17 +37,8 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
-  end
-
-  private
-
-  def set_article
-    @article = Article.find(params[:id])
-  end
-
-  def article_params
-    params.require(:article).permit(:title, :description)
   end
 end
