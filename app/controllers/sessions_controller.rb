4bea2b3fd60683e6289bc970227333f8a56673b4
michele clain <michele.thays@gmail.com>
2022-03-16 21:52:20 -0300

    add delete account option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Loged out"
+    flash[:notice] = "Logged out"
     redirect_to root_path, status: :see_other
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
@@ -32,10 +32,17 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to root_path, status: :see_other
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles succefully deleted"
+    redirect_to root_path, status: :see_other
   end
 
   private
