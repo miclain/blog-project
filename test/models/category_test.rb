c146895249ec42d16bfb20b3fb80815c2b5eef54
michele clain <michele.thays@gmail.com>
2022-04-01 09:14:33 -0300

    add category resources and tests

---
+++ test/models/category_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
+    @category = Category.new(name: "Sports")
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = ""
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "a"
+    assert_not @category.valid?
+  end
+
+  end
+end
