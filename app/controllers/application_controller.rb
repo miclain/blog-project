0d199fc469d04db9b7603f90912db54f4b3d674a
miclain <michele.thays@gmail.com>
2022-04-03 12:44:56 -0300

    add ? in logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :logged_in
+  helper_method :current_user, :logged_in?
   def current_user
     session[:user_id]
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in
+  def logged_in?
     !!current_user
   end
 
   def require_user
-    if !logged_in
+    if !logged_in?
       flash[:alert] = "You must be logged in to perform that action"
       redirect_to login_path
     end
