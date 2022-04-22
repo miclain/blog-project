f3bfd768e641396dbcb1290097dc0d6b254179af
miclain <michele.thays@gmail.com>
2022-04-22 17:32:18 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(username: "Ana", email: "ana@example.com", password: "password", admin: true)
+User.create(username: "Luan", email: "luan@example.com", password: "password", admin: false)
+User.create(username: "Luiza", email: "luiza@example.com", password: "password", admin: false)
+
+Category.create(name: "Tech")
+Category.create(name: "Movies")
+Category.create(name: "Books")
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+Article.create(title: "Seed Tech", description: text, user_id: 1, category_ids: [1])
+Article.create(title: "Seed Movies", description: text, user_id: 2, category_ids: [2])
+Article.create(title: "Seed Books", description: text, user_id: 3, category_ids: [3])
